2eb1cbda40729d6b6fe8dab78b098cb7ba82642c
Stefan677890 <szymonmisztal2626@gmail.com>
2026-04-22 09:07:18 +0200

    Final version
    This version finally works

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -23,11 +23,67 @@ namespace Pong
 
         private void Pong_Load(object sender, EventArgs e)
         {
+            timer1.Interval = 20;
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            if (moveUp && playerY > 0) playerY -= 6;
+            if (moveDown && playerY < this.ClientSize.Height - 80)
+                    playerY += 6;
+
+            if (aiY + 40 < ballY) aiY += 2;
+            if (aiY + 40 > ballY) aiY -= 2;
+
+            if (aiY < 0) aiY = 0;
+            if (aiY > this.ClientSize.Height - 80)
+                aiY = this.ClientSize.Height - 80;
+
+            ballX += ballSpeedX;
+            ballY += ballSpeedY;
+
+            if (ballY <= 0 || ballY >= this.ClientSize.Height - 10)
+                ballSpeedY *= -1;
+
+            Rectangle playerRect = new Rectangle(10, playerY, 10, 80);
+            Rectangle ballRect = new Rectangle(ballX, ballY, 10, 10); ;
+
+            if(playerRect.IntersectsWith(ballRect))
+                ballSpeedX = Math.Abs(ballSpeedX);
+
+            Rectangle aiRect = new Rectangle(this.ClientSize.Width -20, aiY, 10, 80);
+
+            if (aiRect.IntersectsWith(ballRect))
+                ballSpeedX = -Math.Abs(ballSpeedX);
+
+            if (ballX < 0)
+            {
+                aiScore++;
+                Resetball(); 
+            }
+
+            if (ballX > this.ClientSize.Width)
+            {
+                playerScore++;
+                Resetball();
+            }
+
+            Invalidate();
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.FillRectangle(Brushes.White, 10 ,playerY, 10, 80);
+            g.FillRectangle(Brushes.White, this.ClientSize.Width - 20, aiY, 10, 80);
+
+            g.FillEllipse(Brushes.White, ballX, ballY, 10, 10);
+
+            g.DrawString($"Gracz: {playerScore}", new Font("Arial", 12), Brushes.White, 50, 10);
+            g.DrawString($"Ai: {aiScore}", new Font("Arial", 12), Brushes.White, 400, 10);
 
         }
 
@@ -41,6 +97,13 @@ namespace Pong
         {
             if (e.KeyCode == Keys.Up) moveUp = false;
             if (e.KeyCode == Keys.Down) moveDown = false;
+        }
+        private void Resetball()
+        {
+            ballX = this.ClientSize.Width / 2;
+            ballY = this.ClientSize.Height / 2;
+
+            ballSpeedX *= -1;
         }
     }
 }
